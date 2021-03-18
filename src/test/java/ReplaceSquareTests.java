3299ff5e32ca4cc8f6fcfb74212e7e314e06d049
Anna Khalak <Anna.Khalak@simpletexting.net>
2021-03-18 11:00:21 +0300

    Initial commit

---
+++ src/test/java/ReplaceSquareTests.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ReplaceSquareTests {
+
+    @Test
+    void ReplacingTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
